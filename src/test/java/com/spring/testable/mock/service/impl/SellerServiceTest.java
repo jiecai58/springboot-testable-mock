ed36cfb5eec4b346672869b73a858b7581aca3a1
蔡杰 <caijie2@tuhu.cn>
2021-07-12 11:06:45 +0800

    mock 数据校验

--- src/test/java/com/spring/testable/mock/service/impl/SellerServiceTest.java
+++ src/test/java/com/spring/testable/mock/service/impl/SellerServiceTest.java
@@ -1,11 +1,16 @@
 package com.spring.testable.mock.service.impl;
 
+import com.alibaba.testable.core.annotation.MockWith;
 import com.spring.testable.mock.BaseTest;
 import com.spring.testable.mock.service.SellerService;
 import org.junit.Test;
 
 import javax.annotation.Resource;
 
+import static com.alibaba.testable.core.matcher.InvokeVerifier.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MockWith
 public class SellerServiceTest extends BaseTest {
 
     @Resource
@@ -13,14 +18,14 @@ public class SellerServiceTest extends BaseTest {
 
     @Test
     public void should_sell_sandwich() {
-        //"Fake-Sandwich-Cooker & Faked-Sandwich",
-        sellerService.sellSandwich();
+        assertEquals("Fake-Sandwich-Cooker & Faked-Sandwich", sellerService.sellSandwich());
+        verify("hireSandwichCooker").withTimes(1);
     }
 
     @Test
     public void should_sell_hamburger() {
-       //"Real-Hamburger-Cooker & Faked-Hamburger"
-        sellerService.sellHamburger();
+        assertEquals("Real-Hamburger-Cooker & Faked-Hamburger", sellerService.sellHamburger());
+        verify("cookHamburger").withTimes(1);
     }
 
 }
