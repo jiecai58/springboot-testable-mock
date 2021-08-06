af671bb17d6f2bfb0779657353929cac0ef0795a
蔡杰 <caijie2@tuhu.cn>
2021-08-06 11:21:43 +0800

    update

--- src/main/java/com/spring/testable/mock/common/exceptions/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/spring/testable/mock/common/exceptions/GlobalDefaultExceptionHandler.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author Jie.cai58@gmail.com
@@ -24,9 +27,10 @@ public class GlobalDefaultExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
-    public BaseResponse<?> exceptionHandler(Exception e) {
+    public BaseResponse<?> exceptionHandler(HttpServletRequest req, HttpServletResponse response, Exception e) {
         e.printStackTrace();
         if(e instanceof BusinessException){
+            log.error("Exception: from:{} uri:{} code:{} msg:{}", req.getRemoteHost(), req.getRequestURI(), ((BusinessException) e).getCode(), e.getMessage());
             return new BaseResponse(((BusinessException) e).getCode(), e.getMessage());
         }else if(e instanceof MethodArgumentNotValidException){
             return new BaseResponse(ErrorCode.ERROR_SYSTEM.getCode(),methodArgumentNotValidExceptionHandler((MethodArgumentNotValidException)e));
@@ -35,7 +39,20 @@ public class GlobalDefaultExceptionHandler {
         }else if(e instanceof MissingServletRequestParameterException){
             return new BaseResponse(ErrorCode.ERROR_SYSTEM.getCode(), ((MissingServletRequestParameterException) e).getParameterName());
         }
-        return new BaseResponse(ErrorCode.ERROR_SYSTEM.getCode(),e.getMessage()!=null?e.getMessage():ErrorCode.ERROR_SYSTEM.getMessage());
+        return new BaseResponse(ErrorCode.ERROR_SYSTEM.getCode(),optimizationTips(e));
+    }
+
+    private String optimizationTips(Exception e){
+        String message = e.getMessage();
+        if (null == message || message.equals("")) {
+            message = ErrorCode.ERROR_SYSTEM.getMessage();
+        }
+        //优化友好的页面信息提示
+        String[] messages = message.split(System.getProperty("line.separator", "/n"));
+        if (messages != null && messages.length > 0) {
+            message = messages[0];
+        }
+        return message;
     }
 
     private String methodArgumentNotValidExceptionHandler( MethodArgumentNotValidException e) {
