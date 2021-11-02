9d8726bf6ed0c425ab310b64c755f895e056ad71
蔡杰 <caijie2@tuhu.cn>
2021-11-02 13:05:25 +0800

    update

--- src/main/java/com/spring/testable/mock/dao/HeroDao.java
+++ src/main/java/com/spring/testable/mock/dao/HeroDao.java
@@ -1,7 +1,9 @@
 package com.spring.testable.mock.dao;
 
 import com.spring.testable.mock.pojo.entity.Hero;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface HeroDao extends BaseDao<Hero> {
 
     Hero heroinfo(Long id);
